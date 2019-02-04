453d966d378d05929a757694abcd6cb40df8c5bb
Michelle Wang <michelle.wang@warriorlife.net>
2019-02-04 09:17:23 -0800

    try to make new page

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+var startGame = document.querySelector('.startGame');
+var overlay = document.querySelector('#overlay');
+startGame.addEventListener('click', function (e) {
+overlay.style.display = "none";
+});
