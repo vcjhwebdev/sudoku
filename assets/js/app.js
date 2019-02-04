efa6adff7cb4649c2915f1facb51bcbe0cc9df60
Michelle Wang <michelle.wang@warriorlife.net>
2019-02-04 09:51:51 -0800

    trying to make new page and redoing buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var startGame = document.querySelector('.startGame');
 var overlay = document.querySelector('#overlay');
+var game = document.querySelector('#game');
 startGame.addEventListener('click', function (e) {
 overlay.style.display = "none";
 });
