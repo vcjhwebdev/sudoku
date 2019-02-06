94f044f1ff818cf90cb38c32ad328545fc2622a3
Michelle Wang <michelle.wang@warriorlife.net>
2019-02-06 09:25:35 -0800

    trying to make another page with button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
-var startGame = document.querySelector('.startGame');
+var startGame = document.querySelector('.start-game');
 var overlay = document.querySelector('#overlay');
 var game = document.querySelector('#game');
-startGame.addEventListener('click', function (e) {
+
+startGame.addEventListener('click', function(e) {
 overlay.style.display = "none";
+game.style.display = "none";
 });
